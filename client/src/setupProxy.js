00f1f18bb344b4e7ec850e087262597870403746
Gabriel Luis <gabiluisfreitas@gmail.com>
2022-12-12 18:12:32 +0000

    Backend ip solved in frontend proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,13 +1,11 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const config = require('./app/config/backend.config.js');
 
-const API_URL = `http://${config.BACKEND_HOST}:8080`;
-
 module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://10.6.131.24:8080",
+      target: `http://${config.BACKEND_HOST}:8080`,
       changeOrigin: true,
     })
   );
